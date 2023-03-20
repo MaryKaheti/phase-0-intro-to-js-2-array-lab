b2b273f6cda6b42d8b72686a992fa60169b63718
MaryKaheti <kahetikihara@gmail.com>
2023-03-20 16:25:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+beforeEach(function() {cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");})
+// cats = ["Milo", "Otis", "Garfield", "Ralph"];
+function destructivelyAppendCat(name) {cats.push(name)};
+function destructivelyPrependCat(name) {return  cats.unshift(name)};
+function destructivelyRemoveLastCat() {cats.pop()};
+function destructivelyRemoveFirstCat() {cats.shift()};
+function appendCat(name) {return[...cats,name]};
+function prependCat(name) {return[name,...cats]};
+function removeLastCat() {return cats.slice(0,-1);}
+function removeFirstCat() {return cats.slice(1,cats.length);}
